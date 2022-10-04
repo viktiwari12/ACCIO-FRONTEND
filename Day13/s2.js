f185facbba35e0b37dcb96e2eeae9e4d86711298
Vikash Tiwary <59365222+viktiwari12@users.noreply.github.com>
2022-10-04 18:20:59 +0530

    Add files via upload

---
+++ Day13/s2.js
@@ -0,0 +1,9 @@
+// console.log(x) // program ka scope yahi khatam 
+
+var y = 10;
+console.log(fun)
+function fun(){
+    console.log("Hey what's up? ")
+}
+
+console.log(y)
---
+++ Day14/script.js
@@ -0,0 +1,25 @@
+var str = "Today I am learning Strings in JavaScript"
+
+console.log(str);
+
+// length of the string 
+var l = str.length;
+console.log(l);
+
+// to uperCase -> will change the entire string in upper case 
+
+console.log(str.toUpperCase());
+
+// to lowerCase  -> will change the entire string into the lower case 
+console.log(str.toLowerCase());
+
+// accessing the characters 
+var c = str.charAt(3)
+console.log(c);
+
+// trim 
+
+
+var st = "   aodsfoagnfodnab      "
+console.log(st.trim())
+
